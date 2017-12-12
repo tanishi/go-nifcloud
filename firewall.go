22861f45d72e67439a1ca4b6e8f9a764296ae563
tanishi <tanishi503@gmail.com>
2017-12-13 03:30:59 +0900

    Add base code for firewall api

---
+++ firewall.go
@@ -0,0 +1,17 @@
+package nifcloud
+
+func (c *Client) AuthorizeSecurityGroupIngress()               {}
+func (c *Client) CreateSecurityGroup()                         {}
+func (c *Client) DeleteSecurityGroup()                         {}
+func (c *Client) DeregisterInstancesFromSecurityGroup()        {}
+func (c *Client) DescribeSecurityActivities()                  {}
+func (c *Client) DescribeSecurityGroups()                      {}
+func (c *Client) RegisterInstancesWithSecurityGroup()          {}
+func (c *Client) RevokeSecurityGroupIngress()                  {}
+func (c *Client) UpdateSecurityGroup()                         {}
+func (c *Client) UpdateSecurityGroupOption()                   {}
+func (c *Client) DescribeSecurityGroupOption()                 {}
+func (c *Client) NiftyRegisterRoutersWithSecurityGroup()       {}
+func (c *Client) NiftyDeregisterRoutersFromSecurityGroup()     {}
+func (c *Client) NiftyRegisterVpnGatewaysWithSecurityGroup()   {}
+func (c *Client) NiftyDeregisterVpnGatewaysFromSecurityGroup() {}
