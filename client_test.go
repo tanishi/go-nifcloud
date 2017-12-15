14e1afed58878108cc85ba9e68252a5dba357753
tanishi <tanishi503@gmail.com>
2017-12-15 09:59:13 +0900

    Add test for generateSignarureToString

--- client_test.go
+++ client_test.go
@@ -2,14 +2,46 @@ package nifcloud
 
 import (
 	"context"
-	"os"
+	"fmt"
+	"net/http"
+	"net/url"
+	"reflect"
 	"testing"
 )
 
 func TestClient(t *testing.T) {
-	u := os.Getenv("NIFCLOUD_ENDPOINT")
-	accessKey := os.Getenv("NIFCLOUD_ACCESSKEY")
-	secretAccessKey := os.Getenv("NIFCLOUD_SECRET_ACCESSKEY")
+	u := "http://example.com/api"
+	accessKey := "YOUR_ACCSESSKEY"
+	secretAccessKey := "YOUR_SECRET_ACCESSKEY"
+
+	actual, err := NewClient(u, accessKey, secretAccessKey)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	parsedURL, err := url.Parse(u)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := &Client{
+		URL:             parsedURL,
+		HTTPClient:      http.DefaultClient,
+		AccessKeyID:     accessKey,
+		SecretAccessKey: secretAccessKey,
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected: %v, but: %v", expected, actual)
+	}
+}
+
+func TestRequest(t *testing.T) {
+	u := "http://example.com/api"
+	accessKey := "YOUR_ACCSESSKEY"
+	secretAccessKey := "YOUR_SECRET_ACCESSKEY"
 
 	c, err := NewClient(u, accessKey, secretAccessKey)
 
@@ -51,4 +83,21 @@ func TestGenerateSignature(t *testing.T) {
 	if actual != expected {
 		t.Errorf("expected: %v, but: %v", expected, actual)
 	}
+}
+
+func TestGenerateStringToSign(t *testing.T) {
+	method := "GET"
+	endpoint := "http://api.example.com"
+	path := "/api/"
+	query := Query{
+		"Action":           "DescribeInstances",
+		"AccessKeyId":      "YOUR_ACCSESSKEY",
+		"SignatureMethod":  "HmacSHA256",
+		"SignatureVersion": "2",
+		"InstanceId":       "test001",
+		"Description":      "/",
+	}
+
+	actual := generateStringToSign(method, endpoint, path, query)
+	fmt.Println(actual)
 }
