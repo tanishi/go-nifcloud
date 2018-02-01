0fd18e8ac6b1c27198266a6205fc9b3dc120ecfe
tanishi <tanishi503@gmail.com>
2018-02-01 15:36:08 +0900

    Add test

---
+++ server_test.go
@@ -0,0 +1,30 @@
+package nifcloud
+
+import (
+	"context"
+	"os"
+	"testing"
+)
+
+func TestDescribeInstances(t *testing.T) {
+	u := os.Getenv("NIFCLOUD_ENDPOINT")
+	accessKey := os.Getenv("NIFCLOUD_ACCESSKEY")
+	secretAccessKey := os.Getenv("NIFCLOUD_SECRET_ACCESSKEY")
+
+	c, err := NewClient(u, accessKey, secretAccessKey)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	params := &DescribeInstancesInput{
+		InstanceIDs: []string{"onishiTest"},
+	}
+
+	if _, err := c.DescribeInstances(ctx, params); err != nil {
+		t.Error(err)
+	}
+}
